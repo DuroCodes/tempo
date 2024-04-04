8e07ad1c3eb6237dc32f9e438c75b422254cb014
DuroCodes <davidwright13503@gmail.com>
2024-04-04 11:50:41 -0400

    feat: add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "~/components/theme-provider";
 import Navbar from "~/components/ui/navbar";
 import { api } from "~/utils/api";
+import Head from "next/head";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,6 +25,25 @@ const MyApp: AppType<{ session: Session | null }> = ({
       enableSystem
       disableTransitionOnChange
     >
+      <Head>
+        <title>Tempo</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Analyze and discover music you enjoy."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Tempo" />
+        <meta
+          property="og:description"
+          content="Analyze and discover music you enjoy."
+        />
+        <meta property="og:image" content="/banner.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content="/banner.png" />
+      </Head>
       <SessionProvider session={session}>
         <main className={`font-sans ${inter.variable}`}>
           <Navbar />
